f04e2d18123399189323064c876ebbaabdb4c7a5
COMRADE040 <rafiulislam040@gmail.com>
2025-09-17 02:20:54 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+// import './App.css'
+import ChatInput from "./ChatInput"
+function App() {
+
+
+  return (
+    <>
+
+    <ChatInput/>
+
+    </>
+  )
+}
+
+
+
+
+export default App
---
+++ src/ChatInput.jsx
@@ -0,0 +1,9 @@
+export default function ChatInput(){
+    
+  return(
+    <div style={{margin:"20px"}}>
+      <input placeholder="Send a message to Chatbot" size={30}/>
+      <button>Send</button>
+    </div>
+  )
+}
